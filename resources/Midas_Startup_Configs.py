ec76682762c097865e36253d4152ff215d6795b5
Isaac Lee Plovanic <isaac.plovanic@gmail.com>
2019-12-07 08:41:42 -0700

    Add a 'make_musicode' function. Needs some work. Modify 'Midas_Startup_Configs.' Delete extra 'Midas_Startup_Configs'.

--- resources/Midas_Startup_Configs.py
+++ resources/Midas_Startup_Configs.py
@@ -1,21 +1,39 @@
 from importlib import reload
-#import musicode
-#import g
+print("Imported function", reload)
 import mayavi
+print("Imported", mayavi)
 from mayavi import mlab
+print("Imported", mlab)
 import numpy
 import numpy as np
+print("Imported (and as 'np')", numpy)
 import sympy
+print("Imported", sympy)
 import music21
+print("Imported", music21)
 import open3d
+print("Imported", open3d)
 import cv2
+print("Imported", cv2)
 import vtk
+print("Imported", vtk)
 import tvtk
-import music21
+print("Imported", tvtk)
 from midas_scripts import musicode
+print("Imported", musicode)
 from midas_scripts import midiart
+print("Imported", midiart)
 from midas_scripts import midiart3D
+print("Imported", midiart3D)
 from midas_scripts import music21funcs
+print("Imported", music21funcs)
+import os
+print("Imported", os)
+import collections
+print("Imported", collections)
+import re
+print("Imported", re)
+
 #g.mc = musicode.Musicode()
 musicode_path = r"C:\Users\Isaac's\Desktop\Isaacs_Synth_Music_Source_Folder\FL\Workflow\9_Scribed_Musicode_Midi\Musicode Fun!\\"
 midiart_path = r"C:\Users\Isaac's\Desktop\Isaacs_Synth_Music_Source_Folder\FL\Workflow\9_Scribed_Musicode_Midi\Midi-Art Fun!\\"
@@ -23,7 +41,15 @@ colormidiart_path = r"C:\Users\Isaac's\Desktop\Isaacs_Synth_Music_Source_Folder\
 thridiart_path = r"C:\Users\Isaac's\Desktop\Isaacs_Synth_Music_Source_Folder\FL\Workflow\9_Scribed_Musicode_Midi\3idiArt Fun!\\"
 test_path = r"C:\Users\Isaac's\Desktop\Isaacs_Synth_Music_Source_Folder\FL\Workflow\9_Scribed_Musicode_Midi\\"
 scratch_path = r"C:\Users\Isaac's\Desktop\Scratch\\"
-intermediary_path = r"C:\Users\Isaac's\Desktop\Isaacs_Synth_Music_Source_Folder\FL\Workflow\9_Scribed_Musicode_Midi\Midas Intermediary\\"
-
+#intermediary_path = r"C:\Users\Isaac's\Desktop\Isaacs_Synth_Music_Source_Folder\FL\Workflow\9_Scribed_Musicode_Midi\Midas Intermediary\\"
+intermediary_path = r"Midas\resources\intermediary_path\\"
+print("Successfully imported all paths.")
 ##Call
-# --->  exec(open(r"C:\Users\Isaac's\PycharmProjects\MIDASwx\midas_scripts\Midas_Startup_Configs.py").read())
+# import os
+# script = "Midas_Startup_Configs.py"
+# script_path = os.path.dirname(os.path.abspath(script))
+# full_path = script_path + "\\resources\\" + script
+# exec(open(r"%s" % full_path).read())
+
+#import musicode
+#import g
